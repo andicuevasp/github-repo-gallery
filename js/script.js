1fdc252ce6f1783157d05ab5b01410de7118e251
andicuevasp <andicuevasp@gmail.com>
2021-05-27 20:01:11 +0200

    Added git repo list

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const overview = document.querySelector(".overview");
 const username = "andicuevasp";
+const repoListElement = document.querySelector(".repo-list");
 
 const gitUserInfo = async function(){
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
 
-    console.log(data);
+   
 
     showUserInfo(data);
-}
+};
 
 gitUserInfo();
 
@@ -26,4 +27,25 @@ const showUserInfo = function(data) {
   </div> `;
 
   overview.append(userInfo);
+  gitRepoList();
 };
+
+const gitRepoList = async function(){
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepos.json();
+
+  showRepos(repoData);
+};
+
+
+const showRepos = function(repos){
+  for(const repo of repos){
+  const repoItem = document.createElement("li");
+  repoItem.classList.add("repo");
+  repoItem.innerHTML = `<h3>repo.${name}</h3>`;
+
+  repoListItem.append(repoItem);
+
+  }
+};
+
